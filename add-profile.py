842ca5df5216147de8be5fa32943dea0453433fb
James Truitt <jtruitt1@swarthmore.edu>
2024-03-11 15:36:36 -0400

    Create add-profile.py

---
+++ add-profile.py
@@ -0,0 +1,23 @@
+"""
+Add a "Profile Name" field to a bag, so that metadata can be added in Bagger.
+"""
+
+import bagit
+import argparse
+from os.path import exists, isdir
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help='Path to bag directory')
+args = parser.parse_args()
+
+if not exists(args.path):
+    print("Error:", args.path, "cannot be found")
+    quit()
+if not isdir(args.path):
+    print("Error:", args.path, "is not a directory")
+
+print("Opening bag at", args.path, "...")
+bag = bagit.Bag(args.path)
+bag.info['Profile Name'] = 'Swarthmore-FHL'
+bag.save()
+print("Bag modified and saved!")
